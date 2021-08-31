079a0a83c7cf92d60e1da4d4812f9bb1ca06cce0
Junior parks <junior@nationwidesoftware.com>
2021-08-30 20:04:01 -0400

    changes

---
+++ encapsulation.py
@@ -0,0 +1,59 @@
+
+
+
+
+
+
+
+# This is a protected function:
+
+class Protected:
+    def __init__(self):
+        self._protectedVar = 0
+
+
+obj = Protected()
+obj._protectedVar = 34
+print(obj._protectedVar)
+
+
+# This is a private function:
+class Protected:
+    def __init__(self):
+        self._privateVar = 12
+
+    def getPrivate(self):
+        print(self._privateVar)
+
+    def setPrivate(self, private):
+        self_privateVar = private
+        
+# Here is objects that use them both.
+obj = Protected()
+obj.getPrivate()
+obj.setPrivate(23)
+obj.getPrivate()
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
---
+++ phonebook.py
@@ -0,0 +1,12 @@
+
+
+self.lbl_fname = tk.Label(self.master,text='First Name:')
+self.lbl_fname.grid(row=0,column=0,padx=(27,0),pady=(10,0),sticky=N+W)
+self.lbl_lname = tk.Label(self.master,text='last Name:')
+self.lbl_lname.grid(row=2,column=0,padx=(27,0),pady=(10,0),sticky=N+W)
+self.lbl_phone = tk.Label(self.master,text='Phone Number:')
+self.lbl_phone.grid(row=4,column=0,padx=(27,0),pady=(10,0),sticky=N+W)
+self.lbl_email = tk.Label(self.master,text='Email Address:')
+self.lbl_email.grid(row=0,column=0,padx=(27,0),pady=(10,0),sticky=N+W)
+self.lbl_info = tk.Label(self.master,text='Information:')
+self.lbl_info.grid(row=0,column=2,padx=(0,0),pady=(10,0),sticky=N+W)
