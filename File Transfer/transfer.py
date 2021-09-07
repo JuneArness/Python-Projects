44eee8b15901a9a0db239f76ca690483623e9de6
Junior parks <junior@nationwidesoftware.com>
2021-09-07 15:38:25 -0400

    changes

--- File Transfer/transfer.py
+++ File Transfer/transfer.py
@@ -3,16 +3,16 @@
 import shutil
 import os
 
-#set where the source of the files are
-source = 'C:\\Users\\junio\\OneDrive\\Documents\\GitHub\\Python-Projects\\File Transfer\\folder_a.py'
+#set where the source of the folder are
+source = 'C:/Users/junio/OneDrive/Documents/GitHub/Python-Projects/File Transfer/SRC'
 
-#set the destination path to filderB
-destination = 'C:\\Users\\junio\\OneDrive\\Documents\\GitHub\\Python-Projects\\File Transfer\\folder_b.py'
-files = os.listdir(source)
+#set the destination path to folderB
+destination = 'C:/Users/junio/OneDrive/Documents/GitHub/Python-Projects/File Transfer/DEST'
+files = os.listdir(source) 
 
 for i in files:
     #we are saying move the files represented by 'i' to their new destination
-    shutil.move(source+i, destination)
+    shutil.move(source+"/"+i, destination)
 
 import os
 
