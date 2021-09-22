7725a92499c171819f1ea48d85a25b3563bca80b
JuneArness <junior@nationwidesoftware.com>
2021-09-22 13:26:42 -0400

    Add files via upload

--- Class/urls.py
+++ Class/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib import admin
 from django.urls import path
-from .views import home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
